ebb9b179ec6d42dec67eb596dfb595329ca1545e
Ruben Gonzalez <rubengaviles@gmail.com>
2024-01-20 13:10:45 +0100

    cleaned S5P data preprocessing script

--- s5p_data_preprocessing.py
+++ s5p_data_preprocessing.py
@@ -7,19 +7,15 @@ from skimage.transform import resize
 
 
 # Specify if EPA dataset or EEA
-US = False
+DATA_SOURCE = "eea"
 
 # Modify data directory if needed
 DATA_DIR = "/netscratch2/rubengaviles/imp-2023/data"
 # DATA_DIR = "data"
 
 # Determine paths to read samples from and store data
-SAMPLES_PATH = (
-    "samples/samples_S2S5P_2018_2020_epa.csv"
-    if US
-    else "samples/samples_S2S5P_2018_2020_eea.csv"
-)
-SAVE_DIR = "sentinel-5p-epa-numpy-resized" if US else "sentinel-5p-eea-numpy-resized"
+SAMPLES_PATH = f"samples/samples_S2S5P_2018_2020_{DATA_SOURCE}.csv"
+SAVE_DIR = f"sentinel-5p-{DATA_SOURCE}-numpy-resized"
 
 # Read the samples file
 samples_df = pd.read_csv(os.path.join(DATA_DIR, SAMPLES_PATH), index_col="idx")
@@ -29,14 +25,12 @@ samples_df = samples_df[~samples_df["no2"].isna()]
 
 
 for index, row in tqdm(samples_df.iterrows()):
-    # Read attributes of row
+    # Determine path to s5p sample
     s5p_path = row["s5p_path"]
-    station_name = row["AirQualityStation"]
+    s5p_path_full = os.path.join(DATA_DIR, f"sentinel-5p-{DATA_SOURCE}", s5p_path)
 
     # Read Grid data for S5P sample
-    s5p_data = xr.open_dataset(
-        os.path.join(DATA_DIR, "data/sentinel-5p-eea", s5p_path)
-    ).rio.write_crs(4326)
+    s5p_data = xr.open_dataset(s5p_path_full).rio.write_crs(4326)
 
     # Convert to numpy
     s5p_data_numpy = s5p_data.tropospheric_NO2_column_number_density.values.squeeze()
@@ -45,4 +39,5 @@ for index, row in tqdm(samples_df.iterrows()):
     s5p_resized = resize(s5p_data_numpy, (200, 200))
 
     # Save file with station name as file name
+    station_name = row["AirQualityStation"]
     np.save(os.path.join(DATA_DIR, SAVE_DIR, station_name), s5p_data_numpy)
